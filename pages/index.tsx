0ecc038376027031d8e25384124d3228c97c4312
Jiali Zhang <zhangjiali86@gmail.com>
2022-11-30 10:39:10 +0100

    try css in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import Head from "next/head";
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title> Jiali's Next Tutorial</title>
+      </Head>
       <Link href = "/about"> About </Link>
-      <h1> Hi </h1>
+      <h1 className = {styles.homePageTitle}> Hi </h1>
     </div>
   )
 }
